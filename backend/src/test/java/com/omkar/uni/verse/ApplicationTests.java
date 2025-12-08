63041582c769e594f412ff318295e1d9411b531c
omkar <omkarpadalkar21@gmail.com>
2025-12-08 11:51:04 +0530

    Init

---
+++ backend/src/test/java/com/omkar/uni/verse/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omkar.uni.verse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
